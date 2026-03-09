c0a2e62ced54899a232264dfb9ce7ccbbe6357e9
EmmanuelVega21 <130109999+EmmanuelVega21@users.noreply.github.com>
2026-03-09 04:52:01 +0000

    Enums::interactive homework

---
+++ target/debug/Enums_Inter.d
@@ -0,0 +1,1 @@
+/workspaces/Enums-interactive/Enums_Inter/target/debug/Enums_Inter: /workspaces/Enums-interactive/Enums_Inter/src/main.rs
---
+++ target/debug/deps/Enums_Inter-9275a9fb595c6a55.d
@@ -0,0 +1,5 @@
+/workspaces/Enums-interactive/Enums_Inter/target/debug/deps/Enums_Inter-9275a9fb595c6a55.d: src/main.rs
+
+/workspaces/Enums-interactive/Enums_Inter/target/debug/deps/Enums_Inter-9275a9fb595c6a55: src/main.rs
+
+src/main.rs:
